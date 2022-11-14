e6653ea37b0fed6f8e06217079f00a94f3e98ce3
MRcraigchick <craigchick@outlook.com>
2022-11-14 16:56:20 +0100

    Filter route edit

--- routes/trips.js
+++ routes/trips.js
@@ -49,64 +49,38 @@ router.get('/byPartner/:partner', (req, res) => {
   }
 });
 
-/////GET TRIPS FILTERED BY PARAMS
+/////GET TRIPS FILTERED BY QUERYS
 
 router.get('/filter', (req, res) => {
   const filters = req.query;
   console.log(filters);
-  //Array de réponse où push les résultats du filtre
   const response = [];
-  //définition des constantes de mois pour comparer à l'intervale
   let startMonth = Number(filters.startMonth != '' ? filters.startMonth : 1);
   let endMonth = Number(filters.endMonth != '' ? filters.endMonth : 12);
   let minBudget = Number(filters.minBudget ? filters.minBudget : 0);
   let maxBudget = Number(filters.maxBudget ? filters.maxBudget : 30000);
   let searchInput = filters.searchInput != '' ? filters.searchInput : '';
   let tags = filters.tags ? filters.tags : [];
-  console.log(startMonth, endMonth, minBudget, maxBudget);
   Trip.find().then((data) => {
     data.map((trip, i) => {
-      //constantes du trip du catalogue à comparer
       let startMonthTrip = trip.travelPeriod[0].start; //début de la travel period du trip
       let endMonthTrip = trip.travelPeriod[0].end; //fin de la travel period du trip
       let minPriceTrip = trip.program[0].price; //prix du plus court programme = "à partir de...€"
-      //est-ce que le tag fourni en query peut être retrouvé dans le tags Array du Trip (true par défaut si pas de valeur fournie)
-      // let tagMatch = trip.tags.length > 0 && tags.length > 0 ? trip.tags.some(tag => tags.find(e => e==tag)) : true;
       if (
         !response.some((e) => e.id === trip.id) &&
-        //condition pour que le budget matche
         minPriceTrip >= minBudget &&
         minPriceTrip <= maxBudget
-        //condition pour que la travelPeriod matche
-        // &&  startMonth<=startMonthTrip && endMonthTrip <= endMonth
-        // && startMonthTrip <= endMonth && endMonth <= endMonthTrip
-        // && tagMatch
       ) {
         response.push(trip);
       }
     });
 
-    //Si les filtres sont des arrays de strings
-    // else if ((key === 'tags' || key === 'included') && filters[key].length > 0) {
-    //     data.map(trip => {
-    //         if(trip[key].some(e => {
-    //           filters[key].map (el => el.title === e)
-    //         }) && !response.some(e => e.id === trip.id)) {
-    //           console.log(trip.name)
-    //         }
-    //       })
-    // }
-    //pour tous les autres query parameters :
-
-    //si la réponse contient au moins un trip, renvoyer la réponse
     if (response.length > 0) {
       res.json({ result: true, trips: response });
-    }
-    // si la réponse est vide, aucun voyage ne correspond à la recherche
-    else {
+    } else {
       res.json({ result: false, error: 'No trips corresponding to the filters' });
     }
-  })})
-;
+  });
+});
 
 export default router;
